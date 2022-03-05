d2ecf7c029a8d4bf85c62a04bf9b3ada305c496a
ohzqq <iamchurkey@gmail.com>
2022-03-05 07:02:31 -0500

    Add filtering interface

---
+++ exe/commands.rb
@@ -0,0 +1,31 @@
+module CalibreAPI
+  module Commands
+    autoload :List, 'calibre_api/commands/list'
+    autoload :Search, 'calibre_api/commands/search'
+
+    def data(cmd, table, args, options)
+      data = CalibreAPI[table]
+
+      if options.key?(:ids)
+        puts data.data.map(:id).join(",")
+      else
+        fields = options.fetch(:fields).split(",") if options.key?(:fields)
+        case cmd
+        when :list
+          ids = args.first || options.fetch(:ids)
+        when :search
+          query = args.join(" ")
+        end
+
+        data = CalibreAPI[table]
+        data = data.find(ids.split(",")) if ids
+        data = data.query(query) if query
+        data = data.by(options.fetch(:sort)) if options.key?(:sort)
+        data = data.desc if options.key?(:desc)
+
+        #puts args.inspect
+        puts data.meta.as_json(*fields)
+      end
+    end
+  end
+end
