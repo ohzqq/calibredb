05816ffcc767109dfc34e9f92225f82b5c094116
ohzqq <iamchurkey@gmail.com>
2022-03-05 17:01:23 -0500

    No book data for book listing

--- lib/calibredb/model.rb
+++ lib/calibredb/model.rb
@@ -21,20 +21,22 @@ module Calibredb
           default
         end
         
-        def as_json(desc = nil, *associations)
+        def as_json(desc = nil, books = true, *associations)
           as_hash(desc, *associations).to_json
         end
 
-        def as_hash(desc = nil, *associations)
+        def as_hash(desc = nil, books = true, *associations)
           d = desc ? data.reverse : data
           d.map do |row|
             meta = {}
             meta[:value] = row.value
             meta[:id] = row.id
-            if Calibredb.fields.many_books_to_many.to_sym.include?(category) ||
-                Calibredb.fields.one_to_many_books.to_sym.include?(category)
-              meta[:book_ids] = row.books_dataset.map(:id)
-              meta[:total_books] = row.books_dataset.count
+            if books
+              if Calibredb.fields.many_books_to_many.to_sym.include?(category) ||
+                  Calibredb.fields.one_to_many_books.to_sym.include?(category)
+                meta[:book_ids] = row.books_dataset.map(:id)
+                meta[:total_books] = row.books_dataset.count
+              end
             end
             #meta[:url] = "/#{library.name}/#{category}/#{row.id}"
             meta
